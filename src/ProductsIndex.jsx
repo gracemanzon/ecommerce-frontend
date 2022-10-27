1e0f18a50bef7fa8eb14d9bd6329b91c66cc9e2e
grace manzon <86327895+gracemanzon@users.noreply.github.com>
2022-10-27 13:48:49 -0400

    add search/filter and suggested input

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,18 +1,30 @@
+import { useState } from "react";
+
 export function ProductsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   return (
     <div id="products-index">
-      <h1>All Products</h1>
-      <div className="all-products">
+      Search filter:{" "}
+      <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} list="names" />
+      <datalist id="names">
         {props.products.map((product) => (
-          <div key={product.id} className="card custom-card">
-            <h5 className="card-title">{product.name}</h5>
-            <img src={product.images[0]?.url} alt="" />
-            <p></p>
-            <button onClick={() => props.onSelectProduct(product)} className="btn btn-primary">
-              More Info
-            </button>
-          </div>
+          <option key={product.id}>{product.name}</option>
         ))}
+      </datalist>
+      <div className="all-products">
+        {props.products
+          .filter((product) => product.name.toLowerCase().includes(searchFilter.toLowerCase()))
+          .map((product) => (
+            <div key={product.id} className="card custom-card">
+              <h5 className="card-title">{product.name}</h5>
+              <img src={product.images[0]?.url} alt="" />
+              <p></p>
+              <button onClick={() => props.onSelectProduct(product)} className="btn btn-primary">
+                More Info
+              </button>
+            </div>
+          ))}
       </div>
     </div>
   );
